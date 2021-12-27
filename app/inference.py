7e35e1ceb94420bcc300831f027340452e541699
ikky <ikramikky4462@gmail.com>
2021-12-27 18:12:05 +0300

    Changes to inference.py

--- app/inference.py
+++ app/inference.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
+
 class ForestClassificationInference():
     """
     Args:
@@ -9,20 +10,23 @@ class ForestClassificationInference():
     batch_size, predictions
     Notes: Output of this model is 0/1, adapt for multi-class classification
     """
-    def __init__(self, model_path:str) -> None:
+
+    def __init__(self, model_path: str) -> None:
         self.model = tf.saved_model.load(model_path)
         print(f'Model signature(s): {self.model.signatures.keys()}')
         self.infer = self.model.signatures["serving_default"]
         print(self.infer.structured_outputs)
+
     def predict(self, x):
         x = tf.constant(x, tf.float32)
         if len(x.shape) < 4:
             x = tf.expand_dims(x, axis=0)
         x = self.infer(x)
-        #Get dict key from self.infer.structured_outputs
-        x =  np.array(x["output_1"])
+        # Get dict key from self.infer.structured_outputs
+        x = np.array(x["output_1"])
         x = (x == 1).astype(int)
         return x
+
 
 if __name__ == "__main__":
     img = tf.random.uniform([1, 224, 224, 1])
